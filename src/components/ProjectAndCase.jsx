0e10037a95e8bd4d5d95a111e740431bc6dc7d5a
Fadi Khadjar <fadi.khadjar@gmail.com>
2023-11-14 11:27:50 +0100

    testimonial component added to project

--- src/components/ProjectAndCase.jsx
+++ src/components/ProjectAndCase.jsx
@@ -16,29 +16,29 @@ const ProjectAndCase = () => {
                     <h2>Let's Looks Our Global Projects</h2>
                 </div>
                 <div className="project-and-cases">
-                    <a className="project" href="#">
+                    <a className="project" href="NotFound">
                         <img src={articleImage1} alt="A mans hands reading a business paper" />
                         <h3>Grow your business</h3>
                         <div className="readmore">Read More<i className="fa-regular fa-arrow-up-right"></i></div>
                     </a>
-                    <a className="project" href="#">
+                    <a className="project" href="NotFound">
                         <img src={articleImage2} alt="Pink apple products on a desk" />
                         <h3>Why your client needs a responsive website</h3>
                         <div className="readmore">Read More<i className="fa-regular fa-arrow-up-right"></i></div>
                     </a>
-                    <a className="project" href="#">
+                    <a className="project" href="NotFound">
                         <img src={articleImage3} alt="Desk with office supplies" />
                         <h3>Educate your employees to get better results</h3>
                         <div className="readmore">Read More<i className="fa-regular fa-arrow-up-right"></i></div>
                     </a>
-                    <a className="project" href="#">
+                    <a className="project" href="NotFound">
                         <img src={articleImage4} alt="Laptop with Business Intelligence Insights" />
                         <h3>Business Insights is a important piece of your business</h3>
                         <div className="readmore">Read More<i className="fa-regular fa-arrow-up-right"></i></div>
                     </a>
                 </div>
                 <div className="center-content">
-                    <a className="btn-black" href="projects.html">All Recent Projects<i className="fa-regular fa-arrow-up-right"></i></a>
+                    <a className="btn-black" href="NotFound">All Recent Projects<i className="fa-regular fa-arrow-up-right"></i></a>
                 </div>
             </div>
         </section>
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,8 +1,80 @@
 import React from 'react'
+import imageOfCassandra from '../assets/images/image-cassandra.png'
+import imageOfAmanda from '../assets/images/image-amanda.png'
+import imageOfJack from '../assets/images/image-jack.png'
+
+
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <>
+        <section className="testimonial">
+            <div className="container">
+                <div className="center-content">
+                    <div className="section-title">
+                        <p>Testimonial</p>
+                        <h2>What Our Client Says</h2>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="column">
+                        <div className="font-stars">
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                        </div>
+                        <p>"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium libero, ad dignissimos velit qui, dolorum obcaecati cum saepe nesciunt nemo eligendi numquam voluptate"</p>
+                        <div className="clients">
+                            <img src={imageOfCassandra} />
+                            <div>
+                                <h4>Cassandra Warren</h4>
+                                <p>Business Manager, Dorfus</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="font-stars">
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                        </div>
+                        <p>"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium libero, ad dignissimos velit qui, dolorum obcaecati cum saepe nesciunt nemo eligendi numquam voluptate"</p>
+                        <div className="clients">
+                            <img src={imageOfAmanda} />
+                            <div>
+                                <h4>Amanda Tulling</h4>
+                                <p>Senior Developer, Square</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="font-stars">
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                            <i className="fa-sharp fa-solid fa-star"></i>
+                        </div>
+                        <p>"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium libero, ad dignissimos velit qui, dolorum obcaecati cum saepe nesciunt nemo eligendi numquam voluptate"</p>
+                        <div className="clients">
+                            <img src={imageOfJack} />
+                            <div>
+                                <h4>Jack McDogglas</h4>
+                                <p>Key Account Manager, Gobona</p>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+                <div className="center-content">
+                    <a className="btn-black" href="NotFound">All Reviews<i className="fa-regular fa-arrow-up-right"></i></a>
+                </div>
+            </div>
+        </section>
+    </>
   )
 }
 
