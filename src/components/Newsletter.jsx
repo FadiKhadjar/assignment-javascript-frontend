e22f4ea1509e30c30825d2b5962d8dc080feb0a4
Fadi Khadjar <fadi.khadjar@gmail.com>
2023-11-14 11:41:28 +0100

    Newsletter component added to project

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import wavyLines from '../assets/images/background-wavy-lines.svg'
+
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <>
+        <section className="newsletter">
+            <img className="background-wavy-lines" src={wavyLines} alt="" />
+            <div className="container">
+                <h2>Get News Updates By Signup</h2>
+                <form>
+                    <input type="text" placeholder="username@domain.com" />
+                    <button className="btn-yellow">Subscribe<i className="fa-regular fa-arrow-up-right"></i></button>
+                </form>
+            </div>
+        </section>
+    </>
   )
 }
 
