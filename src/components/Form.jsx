273b9654509f7e68dc5353aba71fe493863833a2
Fadi Khadjar <fadi.khadjar@gmail.com>
2023-11-15 18:46:34 +0100

    form validation updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,17 @@
 import { useFormik } from 'formik'
 import React from 'react'
 import { useState } from 'react'
+import * as Yup from 'yup'
 
 
 
 const Form = () => {
 
+    
+
     const [errorMessage, setErrorMessage] = useState('')
+
+    const [validMessage, setValidMessage] = useState('')
 
     const form = useFormik ({
         initialValues: {
@@ -14,6 +19,19 @@ const Form = () => {
             email: '',
             message: ''
         },
+
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required("Please enter your name")
+                .min(2, "The name must have at least 2 characters"),
+            email: Yup.string()
+                .required("Please enter your email address")
+                .email("Please provide a properly formatted email address"),
+            message: Yup.string()
+                .required("You need to enter a message")
+                .min(5, "The message must have at least 5 characters")
+        }),
+
 
         onSubmit: async (values) => {
             const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
@@ -26,16 +44,21 @@ const Form = () => {
 
             switch (result.status) {
                 case 200:
-                    alert('Meddelandet har skickats.')
+                    // alert('Meddelandet har skickats.')
+                    setValidMessage('Your message has been sent successfully')
                     console.log('Meddelandet gick iväg')
                     break;
                 case 400:
-                    setErrorMessage('Något gick fel.')
+                    setErrorMessage('Something is wrong')
+                    console.log('Meddelandet gick inte iväg')
                     break;
+                default:
+                    setErrorMessage('Your message could not be delivered, please try again')
             }
         }
     })
 
+  
 
   return (
     <>
@@ -43,17 +66,21 @@ const Form = () => {
                 <div className="container">
                     <h2>Leave us a message <span>for any information.</span></h2>
                     <form className="registerForm" noValidate onSubmit={form.handleSubmit}>
-                        <p className="errorMessage">{errorMessage}</p>
+                        {/* <p className="errorMessage">{errorMessage}</p> */}
                         <div className="input-field">
-                            <input className="name" type="text" name="name" placeholder="Name*" tabIndex="1" value={form.values.name} onChange={form.handleChange}/>
+                            <label className={(form.touched.name && form.errors.name) ? 'error': ''}>{form.touched.name && form.errors.name  ? form.errors.name : ''}</label>
+                            <input className="name" type="text" name="name" placeholder="Name*" tabIndex="1" value={form.values.name} onChange={form.handleChange} onBlur={form.handleBlur} />
                         </div>
                         <div className="input-field">
-                            <input className="email" type="email" name="email" placeholder="Email*" tabIndex="2" value={form.values.email} onChange={form.handleChange} />
+                            <label className={(form.touched.email && form.errors.email)  ? 'error': ''}>{form.touched.email && form.errors.email  ? form.errors.email : ''}</label>
+                            <input className="email" type="email" name="email" placeholder="Email*" tabIndex="2" value={form.values.email} onChange={form.handleChange} onBlur={form.handleBlur} />
                         </div>
                         <div className="input-field">
-                            <input className="yourMessage" type="text" name="message" placeholder="Your Message*" tabIndex="3" value={form.values.message} onChange={form.handleChange} />
+                            <label className={(form.touched.message && form.errors.message)  ? 'error': ''}>{form.touched.message && form.errors.message ? form.errors.message : ''}</label>
+                            <input className="yourMessage" type="text" name="message" placeholder="Your Message*" tabIndex="3" value={form.values.message} onChange={form.handleChange} onBlur={form.handleBlur} />
                         </div>
                         <button className="btn-yellow" type="submit" >Send Message<i className="fa-regular fa-arrow-up-right"></i></button>
+                        <p className="validMessage">{validMessage}</p>
                     </form>
                 </div>
         </section>
